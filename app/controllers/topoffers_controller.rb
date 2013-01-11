395a8a1e9f16c2f9187364c78a76a998a286a2bf
cw <conway@xerpi.com>
2013-01-11 01:59:15 -0500

    added badcredit, changed compareDesign, added TagManager

--- app/controllers/topoffers_controller.rb
+++ app/controllers/topoffers_controller.rb
@@ -8,7 +8,17 @@ class TopoffersController < ApplicationController
   	render ('prepaid')
   end	
 
+	def badcredit
+  	@page="0012"
+  end
+
   def prepaid
   	@page="0010"
   end
+  	
+  def prepaid_b
+    @page="0013"
+  end
+
+
 end
