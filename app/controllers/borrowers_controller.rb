6e94535403281dcea42d7768cfb50d982560b96a
cw <conway@xerpi.com>
2013-02-24 23:07:10 -0500

    working on borrowers

--- app/controllers/borrowers_controller.rb
+++ app/controllers/borrowers_controller.rb
@@ -8,30 +8,32 @@ class BorrowersController < ApplicationController
 	require 'open-uri'
 
   def new
+  	@borrower = Borrower.new
+  	@requested_amount = params[:requested_amount]
   end
 
   def create
 
-		if @applicant = Applicant.create(params[:applicant])
+		if @borrower = Borrower.create(params[:applicant])
 
 			# set variables
 			# t3 variables
 			@t3header = {"id" => "1.40896", "password" => "NGvDjqwxSeXvAYleYkyW", "client_ip_address" => request.remote_ip, "minimum_price" => "0", "subaccount" => "0000", "test_status" => "sold"}
 
-			@bigmama=params[:applicant].merge(@t3header)
+			@bigmama=params[:borrower].merge(@t3header)
 			@response = Nokogiri::XML(open("http://system.t3leads.com/system/lead_channel/server_post.php",@bigmama))
 			case @response.xpath('//Status').text
 			when "sold"
 				@redirect = @response.xpath('//Redirect').text
-				@applicant.price = @response.xpath('//Price').text
+				@borrower.price = @response.xpath('//Price').text
 			when "reject", "duplicate"
 				@redirect = "http://www.mobilespinner.com"
 			else "error"
 				@redirect = "http://www.mobilespinner.com"
-				@applicant.description = @response.xpath('//Description').text
+				@borrower.description = @response.xpath('//Description').text
 			end
-			@applicant.leadid = @response.xpath('//LeadID').text
-			@applicant.save
+			@borrower.leadid = @response.xpath('//LeadID').text
+			@borrower.save
 
 		else
 		  # error handling
--- app/helpers/borrowers_helper.rb
+++ app/helpers/borrowers_helper.rb
@@ -1,2 +1,39 @@
 module BorrowersHelper
+
+def time_length
+[
+	['5+ Years', '58'],
+	['4+ Years', '48'],
+	['3+ Years', '36'],
+	['2+ Years', '24'],
+	['1+ Year', '12'],
+	['9+ Months', '12'],
+	['6+ Months', '9'],
+	['6 Months', '6'],
+	['5 Months', '5'],
+	['4 Months', '4'],
+	['3 Months', '3'],
+	['2 Months', '2'],
+	['1 Months', '1']
+]
+end
+
+def pay_freq
+[
+	['Every Week', 'WEEKLY'],
+	['Every Two Weeks', 'BIWEEKLY'],
+	['Twice a Month', 'TWICEMONTHLY'],
+	['Monthly', 'MONTHLY']
+]
+end
+
+def call_times
+[
+	['Anytime', 'ANYTIME'],	
+	['Mornings', 'MORNING'],
+	['Afternoons', 'AFTERNOON'],
+	['Evenings', 'EVENING ']
+]
+end
+
 end
