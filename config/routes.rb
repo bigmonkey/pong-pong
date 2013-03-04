1c1abafdde490e5f255e87060749733e0955b116
haley <conway@xerpi.com>
2013-03-04 00:31:31 -0500

    fixed root to homes#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Pdh::Application.routes.draw do
 
+  root :to => 'homes#index'
+
 
   get "applicants/new"
 
@@ -74,7 +76,6 @@ Pdh::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'homes#get'
 
   # See how all your routes lay out with "rake routes"
 
