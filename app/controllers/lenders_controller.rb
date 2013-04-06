481425e22d6e523f4c6e3da0bd47ad8287b210b9
haley <conway@xerpi.com>
2013-04-06 02:03:51 -0400

    hardcoded lender controller url

--- app/controllers/lenders_controller.rb
+++ app/controllers/lenders_controller.rb
@@ -5,15 +5,15 @@ class LendersController < ApplicationController
   before_filter :set_tracking
   
   def index
-    redirect_to("/payday-loans/")
+    redirect_to("http://www.thepaydayhound.com/payday-loans/")
   end
 
   def finder
     @state=State.find(params[:state][:id]).state_abbr.downcase
     if params[:lender_type] == "term" #send to term loans controller
-     redirect_to("/installment-loans/#{@state}?sniff_id=#{params[:sniff_id]}&ranking=#{params[:ranking]}")
+     redirect_to("http://www.thepaydayhound.com/installment-loans/#{@state}?sniff_id=#{params[:sniff_id]}&ranking=#{params[:ranking]}")
     else          # send to payday loans controller
-     redirect_to("/payday-loans/#{@state}?sniff_id=#{params[:sniff_id]}&ranking=#{params[:ranking]}")
+     redirect_to("http://www.thepaydayhound.com/payday-loans/#{@state}?sniff_id=#{params[:sniff_id]}&ranking=#{params[:ranking]}")
     end  
   end
 
