b49a2783de0f56cdb9a5ae05c83cee459711f156
bigmonkey <conway@xerpi.com>
2013-12-05 20:43:13 -0500

    application.rb added trailing_slash true

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module Pdh
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Added by Con Way. Adds a trailing slash
+    config.action_controller.default_url_options = { trailing_slash: true }
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
