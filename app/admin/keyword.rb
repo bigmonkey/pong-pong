8d4ffc432cdfa3e0e48d415d472b2793f7943870
BigMonkey <conway@xerpi.com>
2014-01-13 23:41:20 -0500

    updated keyword table backend

--- app/admin/keyword.rb
+++ app/admin/keyword.rb
@@ -16,4 +16,32 @@ ActiveAdmin.register Keyword do
 
   permit_params :word, :phrase, :state_phrase, :category, :article, :parent_page, :controller
   
+
+  index do
+    column :word
+    column :category
+    column :parent_page
+    column :controller
+    default_actions
+  end
+
+  filter :word
+  filter :controller
+  filter :category
+  filter :parent_page
+  filter :controller
+
+  form do |f|
+    f.inputs "Keyword Details" do
+      f.input :word, :label => "Keyword"
+      f.input :phrase, :label => "Phrase (plural/noun)"
+      f.input :state_phrase, :label => "Select Tag ('Compare Loans')" 
+      f.input :category, :label => "Category (selects copy)",:as => :select, :collection => Keyword.select('category').group('category').order('category ASC').map {|k| k.category}
+      f.input :article, :label => "Article (category = custom, use html)"
+      f.input :parent_page, :label => "Parent Page (linked from)", :as => :select, :collection => Keyword.all.map { |k| k.word }
+      f.input :controller, :as => :select, :collection => Keyword.select('controller').group('controller').order('controller ASC').map {|k| k.controller}
+    end
+    f.actions
+  end
+
 end
