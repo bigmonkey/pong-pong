24085976b2f1643238fe6a8e579c8ef95b637eda
BigMonkey <conway@xerpi.com>
2014-04-18 03:21:23 -0500

    colors

--- app/assets/javascripts/payday_loan_laws.js
+++ app/assets/javascripts/payday_loan_laws.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-	$("#stateAL").css("background-color", "#669966");
+	$("#stateAL").css("background-color", "#4A661B");
 
 	$("#selectorStateBox").mouseover(function(){
 		$("#stateAL").css("background-color", "transparent");
 	});
 
 	$("#selectorStateBox").mouseout(function(){
-		$("#stateAL").css("background-color", "#669966");
+		$("#stateAL").css("background-color", "#4A661B");
 	});
 });
