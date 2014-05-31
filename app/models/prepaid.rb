67de22d7f221138616aab71ab2745deaf3c1d412
BigMonkey <conway@xerpi.com>
2014-05-31 16:35:05 -0500

    change lender box, updated sitemap - avant and kws

--- app/models/prepaid.rb
+++ app/models/prepaid.rb
@@ -3,7 +3,10 @@ class Prepaid < ActiveRecord::Base
 
 
   #scope :by_top_rank, order("prepaids.rating DESC")  
-
+    def self.by_top_rank
+    	order(rating: :desc)
+    end
+    
   # filters out secured cards that we can show live on the site
     def self.live
 		where("live = ?", true)
